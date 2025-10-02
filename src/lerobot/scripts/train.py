c5f8ba306531617d7e26cb785333e76a25fd36c6
Liyou Zhou <liyou.zhou@outlook.com>
2025-10-02 15:15:55 +0100

    Fix an import error

--- src/lerobot/scripts/train.py
+++ src/lerobot/scripts/train.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
+import random
 import time
 from contextlib import nullcontext
 from pprint import pformat
